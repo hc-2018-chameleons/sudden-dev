35fe0bc431a97d298ef95ee4277b8bf9a6fdcd50
Han Thi Nguyen <han.nguyen15@imperial.ac.uk>
2018-01-20 17:04:14 +0000

    fix compile error with app

--- static/js/src/app.js
+++ static/js/src/app.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 
 const rootElement = document.querySelector(document.currentScript.getAttribute('data-container'));
 
-class App() extends React.Component {
-    function render() {
+class App extends React.Component {
+    render() {
+        return <h1>asdfasdf</h1>;
+    }
 }
 
 ReactDOM.render(
