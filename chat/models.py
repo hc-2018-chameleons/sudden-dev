80a48c4a31b10d99f78e0515c5a66c4b489d8cff
Rishabh Jain <wakeuprj@gmail.com>
2018-01-20 18:02:02 +0000

    Added db models for room state and player.

--- chat/models.py
+++ chat/models.py
@@ -3,12 +3,36 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.question_text
+
+class TestCase(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    test_input = models.BinaryField(default=b'')
+    expected_output = models.BinaryField(default=b'')
+
 class Room(models.Model):
     name = models.TextField()
     label = models.SlugField(unique=True)
+    question = models.ForeignKey(Question, on_delete=models.PROTECT, null=True)
+    start_time = models.DateTimeField(default=timezone.now, db_index=True)
+    total_duration = models.IntegerField(default=0)
+    round_start_time = models.DateTimeField(default=timezone.now, db_index=True)
+    round_duration = models.IntegerField(default=5)
 
     def __unicode__(self):
         return self.label
+
+class Player(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.PROTECT, null=True)
+    position = models.IntegerField(null=True)
+    name = models.TextField(null=True)
+    # Add more fields like lines added etc.
+
+    def __str__(self):
+        return self.id
 
 class Message(models.Model):
     room = models.ForeignKey(Room, related_name='messages')
@@ -25,16 +49,4 @@ class Message(models.Model):
 
     def as_dict(self):
         return {'handle': self.handle, 'message': self.message, 'timestamp': self.formatted_timestamp}
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    def __str__(self):
-        return self.question_text
-
-class TestCase(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    test_input = models.BinaryField(default=b'')
-    expected_output = models.BinaryField(default=b'')
-
-
 # Question.objects.all()[0].testcase_set.create(test_input=pickle.dumps(3), expected_output=pickle.dumps(6))
