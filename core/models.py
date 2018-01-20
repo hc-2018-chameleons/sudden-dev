6a5bb6970297f564a6263e80a1fbdb2262d607ce
Rishabh Jain <wakeuprj@gmail.com>
2018-01-20 15:22:53 +0000

    Set up Questions and TestCase database.

--- core/models.py
+++ core/models.py
@@ -3,3 +3,14 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+
+class TestCase(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    test_input = models.CharField(max_length=200)
+    expected_output = models.CharField(max_length=200)
