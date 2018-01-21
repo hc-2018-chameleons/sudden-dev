ff9846d2bfa9901efa5f8b9dcec76528a85d3bae
Han Thi Nguyen <han.nguyen15@imperial.ac.uk>
2018-01-21 01:32:55 +0000

    Game sync logic (thanks Hitesh)

--- static/js/src/reducers/round.js
+++ static/js/src/reducers/round.js
@@ -1,21 +1,45 @@
 import * as actions from "../actions/round";
 
+// Synchronize player order list
+function shiftPlayerList(elapsed, action) {
+    let sw_time = action.round.switch_time;
+    let d_time = action.round.dead_time;
+    let no_of_turns = Math.floor(action.round.time_limit / (sw_time + d_time));
+
+    let cur_turn = Math.floor(elapsed / action.round.time_limit * no_of_turns);
+    let cur_player = cur_turn % action.round.player_ordering.length;
+
+    let players = action.round.player_ordering;
+    for (let i = 0; i < cur_player; i++) {
+        players.push(players.shift());
+    }
+
+    return players;
+}
+
 export default function roundReducer(state = [], action) {
   switch (action.type) {
     case actions.START_ROUND:
       let utc = new Date().getTime();
-      let current_time = Math.min(action.round.time_limit, utc + action.round.time_limit - action.round.starttime_utc);
+      let elapsed = utc - action.round.starttime_utc;
+      let current_time = action.round.time_limit - elapsed;
+
+      let players = shiftPlayerList(elapsed, action);
+
+      let turn_time = elapsed % (action.round.switch_time + action.round.dead_time);
+      let is_sw_time = turn_time >= action.round.dead_time;
+
       return Object.assign({}, state, {
           starttime_utc : action.round.starttime_utc,
           time_limit : action.round.time_limit,
           switch_time : action.round.switch_time,
           dead_time : action.round.dead_time,
-          player_ordering : action.round.player_ordering,
+          player_ordering : players,
           problem : action.round.problem,
           test_cases : action.round.test_cases,
 
           current_time : current_time,
-          is_switch_time : false
+          is_switch_time : is_sw_time
       })
 
     case actions.TIME_TICK:
